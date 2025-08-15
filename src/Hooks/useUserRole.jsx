f023dbcb9a72a160e841d6c16018bb5757dd22e6
moktadir-mirul <moktadir.mirul@gmail.com>
2025-08-15 22:17:57 +0600

    user role issue fixed

--- src/Hooks/useUserRole.jsx
+++ src/Hooks/useUserRole.jsx
@@ -1,22 +1,25 @@
-import { useContext } from "react"
-import { AuthContext } from "../AuthProvider/AuthContext"
+import { useContext } from "react";
+import { AuthContext } from "../AuthProvider/AuthContext";
 import useAxiosSecure from "./useAxiosSecure";
 import { useQuery } from "@tanstack/react-query";
 
 const useUserRole = () => {
-    const {user, pageLoading: AuthLoading} = useContext(AuthContext);
-    const axiosSecure = useAxiosSecure();
-    const {data: role = "user", isLoading: roleLoading, refetch} = useQuery({
-        queryKey:["userrole", user?.email],
-        enabled: !AuthLoading && !!user?.email,
-        queryFn: async () => {
-            const res = await axiosSecure.get(`/users/role?email=${user?.email}`);
-            return res.data.role;
-        }
-    })
+  const { user, pageLoading: AuthLoading } = useContext(AuthContext);
+  const axiosSecure = useAxiosSecure();
+  const {
+    data: role = "user",
+    isLoading: roleLoading,
+    refetch,
+  } = useQuery({
+    queryKey: ["userrole", user?.email],
+    enabled: !AuthLoading && !!user?.email,
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/users/role?email=${user?.email}`);
+      return res.data.role;
+    },
+  });
 
-
-    return {role, roleBasedLoading: AuthLoading || roleLoading, refetch}
-}
+  return { role, roleBasedLoading: AuthLoading || roleLoading, refetch };
+};
 
 export default useUserRole;
